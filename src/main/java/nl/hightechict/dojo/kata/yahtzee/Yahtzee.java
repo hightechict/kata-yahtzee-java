d1900d26063b21cbeb21adf3d81eba2897c8e8b5
Jaime Visser <jaimevisser@gmail.com>
2013-04-11 18:37:32 +0200

    Ones with 2,2,2,2,2 returns 0

--- src/main/java/nl/hightechict/dojo/kata/yahtzee/Yahtzee.java
+++ src/main/java/nl/hightechict/dojo/kata/yahtzee/Yahtzee.java
@@ -1,3 +1,12 @@
 package nl.hightechict.dojo.kata.yahtzee;
 
-public class Yahtzee {}
+public class Yahtzee {
+
+	public enum Category {
+		ONES
+	}
+
+	public int score(int[] is, Category ones) {
+		return 0;
+	}
+}
--- src/test/java/nl/hightechict/dojo/kata/yahtzee/YahtzeeTest.java
+++ src/test/java/nl/hightechict/dojo/kata/yahtzee/YahtzeeTest.java
@@ -1,3 +1,16 @@
 package nl.hightechict.dojo.kata.yahtzee;
 
-public class YahtzeeTest {}
+import nl.hightechict.dojo.kata.yahtzee.Yahtzee.Category;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class YahtzeeTest {
+
+	@Test
+	public void shouldReturnZeroForIncompatibelThrow() {
+		int result = new Yahtzee().score(new int[] { 2, 2, 2, 2, 2 },
+				Category.ONES);
+		Assert.assertEquals(0, result);
+	}
+}
