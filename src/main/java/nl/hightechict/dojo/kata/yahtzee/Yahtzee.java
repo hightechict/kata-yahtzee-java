c3a134afa320daaf70da237f697398f746f7b442
Jaime Visser <jaimevisser@gmail.com>
2013-04-11 18:54:42 +0200

    Ones 12222 returns 1

--- src/main/java/nl/hightechict/dojo/kata/yahtzee/Yahtzee.java
+++ src/main/java/nl/hightechict/dojo/kata/yahtzee/Yahtzee.java
@@ -1,12 +1,16 @@
 package nl.hightechict.dojo.kata.yahtzee;
 
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
 public class Yahtzee {
 
 	public enum Category {
 		ONES
 	}
 
-	public int score(int[] is, Category ones) {
-		return 0;
+	public int score(Iterable<Integer> dice, Category ones) {
+		Multiset<Integer> diceThrow = HashMultiset.create(dice);
+		return diceThrow.count(1);
 	}
 }
--- src/test/java/nl/hightechict/dojo/kata/yahtzee/YahtzeeTest.java
+++ src/test/java/nl/hightechict/dojo/kata/yahtzee/YahtzeeTest.java
@@ -3,14 +3,32 @@ package nl.hightechict.dojo.kata.yahtzee;
 import nl.hightechict.dojo.kata.yahtzee.Yahtzee.Category;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.google.common.collect.Lists;
 
 public class YahtzeeTest {
 
+	Yahtzee yahtzee;
+
+	@Before
+	public void setUp() {
+		yahtzee = new Yahtzee();
+	}
+
 	@Test
-	public void shouldReturnZeroForIncompatibelThrow() {
-		int result = new Yahtzee().score(new int[] { 2, 2, 2, 2, 2 },
+	public void onesShouldReturnZeroForIncompatibelThrow() {
+		int result = yahtzee.score(Lists.newArrayList(2, 2, 2, 2, 2),
 				Category.ONES);
 		Assert.assertEquals(0, result);
+	}
+
+	@Test
+	public void onesShouldReturnOneForSingleOneDice() {
+		int result = yahtzee.score(Lists.newArrayList(1, 2, 2, 2, 2),
+				Category.ONES);
+
+		Assert.assertEquals(1, result);
 	}
 }
